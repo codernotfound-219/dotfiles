bf02f64c4400c12533ca567d6ed78f71a2bf2d53
codernotfound-219 <aswatt219@icloud.com>
2024-12-20 17:57:23 +0530

    feat: Comptetitve-Programming for nvim

---
+++ nvim/lua/plugins/competitive-programming.lua
@@ -0,0 +1,8 @@
+return {
+    "A7lavinraj/assistant.nvim",
+    dependencies = { "stevearc/dressing.nvim" }, -- optional but recommended
+    keys = {
+        { "<leader>cp", "<cmd>AssistantToggle<cr>", desc = "Toggle Assistant.nvim window" }
+    },
+    opts = {}
+}
