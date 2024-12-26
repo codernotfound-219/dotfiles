5e7db7393150db46cfb52a2bc1467f74a27080f7
codernotfound-219 <aswatt219@icloud.com>
2024-12-26 19:44:12 +0530

    update: lazy nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
-    {import = 'plugins'},
+    { import = 'plugins' },
   },
+  checker = { enabled = true },
 })
