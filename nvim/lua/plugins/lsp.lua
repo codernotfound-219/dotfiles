a6d706a55171be4d30994c47f49a674abf974dfb
codernotfound-219 <aswatt219@icloud.com>
2025-06-10 23:48:33 +0530

    feat: smear-cursor + fix: lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,51 +6,47 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    {"j-hui/fidget.nvim",  opts = {} },
+    { "j-hui/fidget.nvim", opts = {} },
   },
 
   config = function()
     require("mason").setup()
+    require("mason-lspconfig").setup()
 
-    -- ADD LSP SERVERS AS REQUIRED
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = {
-              -- SET: vim as global, {annoyin af}
-              globals = {
-                "vim",
-                "Snacks",
-              },
-            },
-          },
-        },
-      },
-      clangd = {},
-      eslint = {},
-      ts_ls = {},
-    }
+    vim.lsp.enable("lua_ls")
+    vim.lsp.enable("clangd")
+    vim.lsp.enable("eslint")
+    vim.lsp.enable("ts_ls")
+    vim.lsp.enable("rust_analyzer")
 
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "eslint",
-        "ts_ls",
-        "lua_ls",
-        "clangd",
-        "rust_analyzer",
-      },
-      handlers = {
-        function (server_name) --Default Handler
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-          require("lspconfig")[server_name].setup(server)
-        end
-      },
+    vim.lsp.config('*', {
+      capabilities = capabilities,
     })
 
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local map = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = 'Lsp: ' .. desc })
+        end
+        local tele = require("telescope.builtin")
+
+        map('gd', tele.lsp_definitions, 'Goto Definition')
+        map('<leader>ts', tele.lsp_document_symbols, 'Doc Symbols')
+        map('<leader>tS', tele.lsp_dynamic_workspace_symbols, 'Dynamic Symbols')
+
+        map('K', vim.lsp.buf.hover, 'hover')
+        map('<leader>E', vim.diagnostic.open_float, 'diagnostic')
+        map('<leader>k', vim.lsp.buf.signature_help, 'sig help')
+        map('<leader>rn', vim.lsp.buf.rename, 'rename')
+        map('<leader>ca', vim.lsp.buf.code_action, 'code action')
+        map('<leader>wf', vim.lsp.buf.format, 'format')
+
+        map('[d', vim.diagnostic.goto_prev, 'previous diagnostic')
+        map(']d', vim.diagnostic.goto_next, 'next diagnostic')
+      end,
+    })
     require("lspconfig.ui.windows").default_options.border = "single"
   end
 }
